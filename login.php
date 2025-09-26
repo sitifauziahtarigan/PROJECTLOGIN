6ff517e2f4c11d993b0b30d7a59f7b3f631c3ef1
sitifauziahtarigan <sitifauziah1218@gmail.com>
2025-09-26 10:33:23 +0700

    Mendesain Form Login

---
+++ login.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
+</head>
+<body>
+    <h2>Form Login</h2>
+    <form method="post">
+        Username: <input type="text" name"username" required><br></br>
+        Password: <input type="Password" name"password" required><br></br>
+        <button type="submit">Login</button>
+        
+</form>
+</body>
+</html>
